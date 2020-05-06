9964f213b1cbf8b748309f34c050b4a943482828
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-05-06 19:46:20 +0530

    added sliders

--- lib/main.dart
+++ lib/main.dart
@@ -26,10 +26,66 @@ class MyApp extends StatelessWidget {
       ),
     );
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Stack(
-        children: <Widget>[],
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: AppBody(),
+      ),
+    );
+  }
+}
+
+class AppBody extends StatefulWidget {
+  AppBody({Key key}) : super(key: key);
+
+  @override
+  _AppBodyState createState() => _AppBodyState();
+}
+
+class _AppBodyState extends State<AppBody> {
+  double angle = 0, height = 0;
+  Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
+
+    return Stack(
+      children: <Widget>[
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: customSlider(),
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: customSlider(shouldRotate: true),
+        ),
+        Padding(
+          padding: EdgeInsets.all(size.width / 5 * 2),
+          child: CustomPaint(
+            child: null,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget customSlider({bool shouldRotate = false}) {
+    return RotatedBox(
+      quarterTurns: shouldRotate ? -1 : 0,
+      child: Container(
+        width: size.width / 1.5,
+        height: size.width / 5,
+        child: Slider(
+          value: shouldRotate ? height : angle,
+          onChanged: (newValue) => setState(
+            () => shouldRotate ? height = newValue : angle = newValue,
+          ),
+        ),
       ),
     );
   }
