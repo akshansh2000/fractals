226fb1608753f435fe14560ad8728519955ff3b3
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-05-07 12:14:34 +0530

    added tree

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class AppBody extends StatefulWidget {
 }
 
 class _AppBodyState extends State<AppBody> {
-  double angle = pi / 4, height = 5;
+  double angle = pi / 4, height = 10, scale = 0.5;
   Size size;
 
   @override
@@ -53,29 +53,30 @@ class _AppBodyState extends State<AppBody> {
 
     return Scaffold(
       backgroundColor: Colors.white10,
-      body: CustomPaint(
-        painter: FractalPainter(),
-        child: Container(),
+      body: Transform.scale(
+        scale: scale,
+        child: CustomPaint(
+          painter: FractalPainter(height, angle),
+          child: Container(),
+        ),
       ),
-      bottomNavigationBar: Container(
-        height: size.height / 6,
-        child: BottomAppBar(
-          elevation: 0,
-          color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              customSlider(),
-              customSlider(isHeight: true),
-            ],
-          ),
+      bottomNavigationBar: BottomAppBar(
+        elevation: 0,
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            customSlider(),
+            customSlider(isHeight: true),
+            customSlider(isScale: true),
+          ],
         ),
       ),
     );
   }
 
-  Widget customSlider({bool isHeight = false}) {
+  Widget customSlider({bool isHeight = false, bool isScale = false}) {
     return Row(
       mainAxisSize: MainAxisSize.max,
       mainAxisAlignment: MainAxisAlignment.center,
@@ -83,17 +84,21 @@ class _AppBodyState extends State<AppBody> {
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
           child: Icon(
-            isHeight ? Icons.linear_scale : Icons.rotate_90_degrees_ccw,
+            isHeight
+                ? Icons.linear_scale
+                : isScale ? Icons.zoom_in : Icons.rotate_90_degrees_ccw,
           ),
         ),
         Slider(
           min: 0,
-          max: isHeight ? 30 : pi,
+          max: isHeight ? 20 : isScale ? 1 : pi,
           activeColor: Colors.red[400],
           inactiveColor: Colors.grey[200],
-          value: isHeight ? height : angle,
+          value: isHeight ? height : isScale ? scale : angle,
           onChanged: (newValue) => setState(
-            () => isHeight ? height = newValue : angle = newValue,
+            () => isHeight
+                ? height = newValue
+                : isScale ? scale = newValue : angle = newValue,
           ),
         ),
       ],
@@ -101,9 +106,48 @@ class _AppBodyState extends State<AppBody> {
   }
 }
 
+int counter = 0;
+
 class FractalPainter extends CustomPainter {
+  Canvas _canvas;
+  Paint _paint;
+  double _height, _angle;
+
+  FractalPainter(this._height, this._angle);
+
   @override
-  void paint(Canvas canvas, Size size) {}
+  void paint(Canvas canvas, Size size) {
+    _canvas = canvas;
+    _paint = Paint()
+      ..color = Colors.brown[800]
+      ..strokeWidth = 5
+      ..strokeJoin = StrokeJoin.bevel
+      ..style = PaintingStyle.stroke;
+
+    canvas.translate(size.width / 2, size.height - 30);
+    drawBranch(_height * _height);
+  }
+
+  void drawBranch(double length) {
+    _canvas.drawLine(Offset(0, 0), Offset(0, -length), _paint);
+    _canvas.translate(0, -length);
+
+    if (counter == length.round()) return;
+
+    if (length > 4) {
+      _canvas.save();
+      _canvas.rotate(_angle);
+      ++counter;
+      drawBranch(length * 0.7);
+      _canvas.restore();
+
+      _canvas.save();
+      _canvas.rotate(-_angle);
+      ++counter;
+      drawBranch(length * 0.7);
+      _canvas.restore();
+    }
+  }
 
   @override
   bool shouldRepaint(FractalPainter oldDelegate) => false;
